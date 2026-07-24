e4da8fb6a9b3f17b48cb80ebcafff148f2d73c0c
hyndaicustomercare-debug <hyndaicustomercare@gmail.com>
2026-07-24 20:34:05 +1000

    Update properties.js

--- properties.js
+++ properties.js
@@ -1,1 +1,312 @@
+// STAR WARS MONOPOLY CLASSIC TRILOGY EDITION
+// PROPERTY DATABASE
+// Replace placeholder names with the exact Australian 1997 board data
 
+
+let properties = [
+
+    {
+        name:"Property 1",
+        colour:"Brown",
+        price:60,
+        mortgage:30,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 2",
+        colour:"Brown",
+        price:60,
+        mortgage:30,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 3",
+        colour:"Light Blue",
+        price:100,
+        mortgage:50,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 4",
+        colour:"Light Blue",
+        price:100,
+        mortgage:50,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 5",
+        colour:"Light Blue",
+        price:120,
+        mortgage:60,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 6",
+        colour:"Pink",
+        price:140,
+        mortgage:70,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 7",
+        colour:"Pink",
+        price:140,
+        mortgage:70,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 8",
+        colour:"Pink",
+        price:160,
+        mortgage:80,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 9",
+        colour:"Orange",
+        price:180,
+        mortgage:90,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 10",
+        colour:"Orange",
+        price:180,
+        mortgage:90,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 11",
+        colour:"Orange",
+        price:200,
+        mortgage:100,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 12",
+        colour:"Red",
+        price:220,
+        mortgage:110,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 13",
+        colour:"Red",
+        price:220,
+        mortgage:110,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 14",
+        colour:"Red",
+        price:240,
+        mortgage:120,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 15",
+        colour:"Yellow",
+        price:260,
+        mortgage:130,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 16",
+        colour:"Yellow",
+        price:260,
+        mortgage:130,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 17",
+        colour:"Yellow",
+        price:280,
+        mortgage:140,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 18",
+        colour:"Green",
+        price:300,
+        mortgage:150,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 19",
+        colour:"Green",
+        price:300,
+        mortgage:150,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 20",
+        colour:"Green",
+        price:320,
+        mortgage:160,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 21",
+        colour:"Dark Blue",
+        price:350,
+        mortgage:175,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Property 22",
+        colour:"Dark Blue",
+        price:400,
+        mortgage:200,
+        owner:null,
+        mortgaged:false,
+        houses:0,
+        hotel:false
+    },
+
+
+    {
+        name:"Starship 1",
+        type:"Starship",
+        price:200,
+        mortgage:100,
+        owner:null,
+        mortgaged:false
+    },
+
+
+    {
+        name:"Starship 2",
+        type:"Starship",
+        price:200,
+        mortgage:100,
+        owner:null,
+        mortgaged:false
+    },
+
+
+    {
+        name:"Utility 1",
+        type:"Utility",
+        price:150,
+        mortgage:75,
+        owner:null,
+        mortgaged:false
+    },
+
+
+    {
+        name:"Utility 2",
+        type:"Utility",
+        price:150,
+        mortgage:75,
+        owner:null,
+        mortgaged:false
+    }
+
+
+];
